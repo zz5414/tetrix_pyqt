7ad8b1ff8c6635d2fe80b33e8d515a1ad6f6fc89
heejin.park <zzqq5414@gmail.com>
2019-07-29 12:19:51 +0900

    여백 없이 그리기 완성

--- making/main.py
+++ making/main.py
@@ -6,6 +6,9 @@ from PyQt5.QtWidgets import QFrame
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtCore import QBasicTimer
 
+from PyQt5.QtGui import QPainter
+from PyQt5.QtGui import QColor
+
 
 class Tetris(QMainWindow):
     def __init__(self):
@@ -21,12 +24,18 @@ class Tetris(QMainWindow):
 
         tetris_board.start()
 
+        self.resize(180, 380)
+
         self.show()
 
 class TetrisBoard(QFrame):
     msg_to_statusbar = pyqtSignal(str)
 
     Speed = 300
+    BoardWidth = 10
+    BoardHeight = 22
+
+
 
     def __init__(self, parent):
         super().__init__(parent)
@@ -34,7 +43,7 @@ class TetrisBoard(QFrame):
         # member variable
         self.timer = QBasicTimer()
 
-
+        self._test_int = 1
 
     def start(self):
         self.msg_to_statusbar.emit("Hello World")
@@ -42,10 +51,40 @@ class TetrisBoard(QFrame):
 
     def timerEvent(self, event):
         if event.timerId() == self.timer.timerId():
-            import time
-            self.msg_to_statusbar.emit(str(time.time()))
+            self.msg_to_statusbar.emit(str(self._test_int))
+            self._test_int += 1
+            self.update()
         else:
             super(TetrisBoard, self).timerEvent(event)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        rect = self.contentsRect()
+
+        #가장 아랫부분까지 채울 수 있도록 정해진 크기만큼 height를 가지도록 윗부분을 자른다.(특정 윗부분을 사용하지 않겠다는 의미)
+        boardTop = rect.height() - TetrisBoard.BoardHeight * self.squareHeight()
+
+        for h_index in range(TetrisBoard.BoardHeight):
+            for w_index in range(TetrisBoard.BoardWidth):
+                import random
+                shape = random.randint(1, 7)
+                colorTable = [0x000000, 0xCC6666, 0x66CC66, 0x6666CC,
+                              0xCCCC66, 0xCC66CC, 0x66CCCC, 0xDAAA00]
+                x = rect.left() + w_index * self.squareWidth()
+                y = boardTop + h_index * self.squareHeight()
+                painter.fillRect(x, y, self.squareWidth(), self.squareHeight(), QColor(colorTable[shape]))
+
+
+
+    def squareWidth(self):
+        '''returns the width of one square'''
+        # print(self.contentsRect().width(), TetrisBoard.BoardWidth, self.contentsRect().width() // TetrisBoard.BoardWidth)
+        return self.contentsRect().width() // TetrisBoard.BoardWidth
+
+    def squareHeight(self):
+        '''returns the height of one square'''
+        # print(self.contentsRect().height(), TetrisBoard.BoardHeight, self.contentsRect().height() // TetrisBoard.BoardHeight)
+        return self.contentsRect().height() // TetrisBoard.BoardHeight
 
 
 
